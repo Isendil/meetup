a7d37f00c555e8688385b52d05d4f7c547c8257c
Dimitrios Ellinidis <isendil_1@yahoo.com>
2020-05-01 17:02:05 -0400

    Updates

---
+++ precache-manifest.60a3d28aa17797825b205caf1a7c4daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba90d9c17316a2056558180cbcd9a19c",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "d3d7d25bbd41ce04af30",
+    "url": "/meetup/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3d8043b3d07056707123",
+    "url": "/meetup/static/js/2.08d4b476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meetup/static/js/2.08d4b476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d7d25bbd41ce04af30",
+    "url": "/meetup/static/js/main.7154864e.chunk.js"
+  },
+  {
+    "revision": "e12257fd0ebca8c655b0",
+    "url": "/meetup/static/js/runtime-main.59475e37.js"
+  }
+]);
