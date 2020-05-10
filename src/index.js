c367a4eebc612adc6332050b4644efe7ce2fc240
Dimitrios Ellinidis <isendil_1@yahoo.com>
2020-05-10 18:47:37 -0400

    commit changes for 4.8

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 atatus.config("b3e658d586db4fd68835b440d21e4421").install();
 atatus.notify(new Error("Test Atatus Setup"));
